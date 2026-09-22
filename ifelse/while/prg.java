cb5ad071464a5bdd2c9b885a0db4e5fa3f717bcc
roopasaividya123ui <beemisettyroopasaividya@gmail.com>
2026-09-22 14:03:33 +0530

    While lopp

---
+++ ifelse/while/prg.java
@@ -0,0 +1,12 @@
+public class prg {
+    public static void main(String[] args){
+        int n=10;
+        int i=3;
+        while(i<=n){
+            System.out.println(i);
+            i=i+2;
+           
+        }
+    }
+    
+}
